872bc1cd89bd25d6b3cc525ff5651954aebbcf3b
José Luis Ferrete <josel.ferrete@s-dos.es>
2020-06-30 16:48:54 +0200

    Fixed issues

--- src/main/java/com/formacionbdi/springboot/app/zuul/filters/PostTiempoTranscurridoFilter.java
+++ src/main/java/com/formacionbdi/springboot/app/zuul/filters/PostTiempoTranscurridoFilter.java
@@ -2,6 +2,7 @@ package com.formacionbdi.springboot.app.zuul.filters;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
@@ -9,14 +10,11 @@ import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
 
-import org.slf4j.Logger;
-
-
 @Component
-public class PostTiempoTranscurridoFilter extends ZuulFilter {
-
-	private static Logger log = LoggerFactory.getLogger(PostTiempoTranscurridoFilter.class);
+public class PostTiempoTranscurridoFilter extends ZuulFilter{
 	
+	private static Logger log = LoggerFactory.getLogger(PostTiempoTranscurridoFilter.class);
+
 	@Override
 	public boolean shouldFilter() {
 		return true;
@@ -24,19 +22,18 @@ public class PostTiempoTranscurridoFilter extends ZuulFilter {
 
 	@Override
 	public Object run() throws ZuulException {
-		
-		
+
 		RequestContext ctx = RequestContext.getCurrentContext();
 		HttpServletRequest request = ctx.getRequest();
-		log.info("Entrando a post filter");
 		
+		log.info("Entrando a post filter");
+				
 		Long tiempoInicio = (Long) request.getAttribute("tiempoInicio");
 		Long tiempoFinal = System.currentTimeMillis();
 		Long tiempoTranscurrido = tiempoFinal - tiempoInicio;
 		
-		log.info(String.format("Tiempo transcurrido en segundos %s", tiempoTranscurrido.doubleValue()/1000.00));
-		log.info(String.format("Tiempo transcurrido en milisegundos %s", tiempoTranscurrido.doubleValue()));
-		
+		log.info(String.format("Tiempo transcurrido en segundos %s seg.", tiempoTranscurrido.doubleValue()/1000.00));
+		log.info(String.format("Tiempo transcurrido en mileseg %s ms.", tiempoTranscurrido));
 		return null;
 	}
 
--- src/main/java/com/formacionbdi/springboot/app/zuul/filters/PreTiempoTranscurridoFilter.java
+++ src/main/java/com/formacionbdi/springboot/app/zuul/filters/PreTiempoTranscurridoFilter.java
@@ -2,6 +2,7 @@ package com.formacionbdi.springboot.app.zuul.filters;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
@@ -9,14 +10,11 @@ import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
 
-import org.slf4j.Logger;
-
-
 @Component
-public class PreTiempoTranscurridoFilter extends ZuulFilter {
-
-	private static Logger log = LoggerFactory.getLogger(PreTiempoTranscurridoFilter.class);
+public class PreTiempoTranscurridoFilter extends ZuulFilter{
 	
+	private static Logger log = LoggerFactory.getLogger(PreTiempoTranscurridoFilter.class);
+
 	@Override
 	public boolean shouldFilter() {
 		return true;
@@ -24,10 +22,10 @@ public class PreTiempoTranscurridoFilter extends ZuulFilter {
 
 	@Override
 	public Object run() throws ZuulException {
-		
-		
+
 		RequestContext ctx = RequestContext.getCurrentContext();
 		HttpServletRequest request = ctx.getRequest();
+		
 		log.info(String.format("%s request enrutado a %s", request.getMethod(), request.getRequestURL().toString()));
 		
 		Long tiempoInicio = System.currentTimeMillis();
